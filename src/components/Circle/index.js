559f2498e21091d65a13e3b0c539784b688443af
Aftaza <taftazani17@student.ub.ac.id>
2023-10-16 16:09:49 +0700

    Fixing naming

--- src/components/Circle/index.js
+++ src/components/Circle/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Circle1 from "^/Circle.svg"
+import Circlee from "^/Circle.svg"
 import Image from 'next/image'
 
 const Circle = () => {
   return (
     <div className="absolute top-0 w-40 h-auto">
-        <Image src={Circle1} alt="The Bloops" width={533} height={449} />
+        <Image src={Circlee} alt="The Bloops" width={533} height={449} />
     </div>
   )
 }
